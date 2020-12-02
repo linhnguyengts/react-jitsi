cc128f76a078fc54a4c7163a06bc20549ef30dc6
Su Su <48374100+linhnguyengts@users.noreply.github.com>
2020-12-02 08:53:15 +0700

    Update defaults.tsx
    change domain to custom server

--- src/defaults.tsx
+++ src/defaults.tsx
@@ -4,7 +4,7 @@ import * as Type from './types'
 export const Loader = (): React.ReactElement => <div>Loading meeting...</div>
 
 export const Props: Type.Props = {
-    domain: 'meet.jit.si',
+    domain: 'ik1-426-45048.vs.sakura.ne.jp',
     roomName: (Math.random() + 0.48151642).toString(36).substring(2),
 }
 
